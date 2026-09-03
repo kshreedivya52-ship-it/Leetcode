faadab66ed059e4551e94911bd384c6d8e966fd1
K Shreedivya <kshreedivya52@gmail.com>
2026-09-03 13:40:24 +0530

    Time: 1 ms (66.29%) | Memory: 19.7 MB (76.78%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseString(self, s):
+        l = 0
+        r = len(s) -1
+
+        while l < r:
+            s[l], s[r] = s[r] , s[l]
+            l += 1
+            r -= 1
+        return s[r]    
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        

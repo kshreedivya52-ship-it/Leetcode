cec11f2693321ec8f25d4c83dc32074f23928954
K Shreedivya <kshreedivya52@gmail.com>
2026-09-07 14:27:43 +0530

    Time: 5 ms (67.98%) | Memory: 13.6 MB (50.22%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                if k != i:
+                    nums[i] = 0
+                k += 1
+
+        return nums       
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        

1ed285276f174d5db39ddcb30adac33782c8a924
K Shreedivya <kshreedivya52@gmail.com>
2026-09-09 20:42:39 +0530

    Time: 1 ms (21.68%) | Memory: 48.3 MB (55.11%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void reverseString(char[] s) {
+        StringBuilder reverse = new StringBuilder(new String(s)).reverse();
+          for(int i = 0; i<s.length; i++){
+            s[i] = reverse.charAt(i);
+          }
+    }
+}
